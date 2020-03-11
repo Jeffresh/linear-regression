a694e4dc5a70838af4a06de2a38ec838326484a9
Jeff <jeff.uca@gmail.com>
2020-03-11 22:34:13 +0100

    [MOD] minor changes in plot visualization

--- octave-matlab/plot_data.m
+++ octave-matlab/plot_data.m
@@ -2,6 +2,7 @@ function  plot_data (x,y)
 %plot_data Plots the data.
 
 plot(x,y,'rx','MarkerSize', 10);
+axis([0,100,0, 300])
 ylabel('Heart rate: beats per minute');
 xlabel('Body Weight: Kg'); 
 
